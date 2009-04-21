1f6799b0873a3e1d07a3d1e651dc41d15724ce6c
Stephan Pavlovic <stephanpavlovic@stephan-pavlovics-computer-4.local>
2009-04-22 05:20:14 +0800

    Kommentare angefangen
    Signed-off-by: kangguru <development@aufnahme.com>

--- lib/semantic_record/support.rb
+++ lib/semantic_record/support.rb
@@ -1,15 +1,20 @@
 module SemanticRecord
+  # Extents Ruby-classes with some usefull methods
   module Support
     Array.class_eval do
+      
+      # Extents *Array* with a method that takes every Array-element and adds a *?* -> for Sparql
       def to_sparql_properties
         self.size > 0 ? "?#{self.join(' ?')}" : ""
       end
-      
+    
+      # Extents *Array* with a method that reduces a array to his first element
       def first!
         a = self.first
         self.clear << a
       end
       
+      # Extents *Array* with a method that reduces a array to his last element
       def last!
         a = self.last
         self.clear << a
@@ -17,15 +22,16 @@ module SemanticRecord
     end
     
     Hash.class_eval do
+      
+      # Extents *Hash* with a method that creates OPTIONAL-clauses for every element of the Hash -> for Sparql
       def to_optional_clause
         properties = collect {|k,v| "OPTIONAL { ?uri <#{k}> ?#{k.to_human_name}. }" }.join(' ')
-        
-#        properties!="" ? "?uri #{properties}" : ""
       end      
     end    
     
     String.class_eval do
       # TODO give me a sleeker name 
+      # Extents *String* with a method that strips out the namespace of a string
       def to_human_name
         self.split('#').size > 1 ? self.split('#').last : self.split('/').last
       end
